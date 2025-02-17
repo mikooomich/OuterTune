5b404a3a13d8ffe4829b87458a83dc0ea50a9825
mikooomich <mikooomichael@gmail.com>
2025-02-16 22:38:38 -0500

    DNM: Use local extractor

--- innertube/build.gradle.kts
+++ innertube/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation(libs.ktor.serialization.json)
     implementation(libs.ktor.client.encoding)
     implementation(libs.brotli)
-    implementation(libs.newpipe.extractor)
+    implementation("com.github.teamnewpipe:NewPipeExtractor")
+//    implementation(libs.newpipe.extractor)
     testImplementation(libs.junit)
 }
