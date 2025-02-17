5a7070ee3e0fee3c8d8c42c0a6ca94ff37f0fc98
mikooomich <mikooomichael@gmail.com>
2025-02-16 20:37:41 -0500

    multiqueue: No seamless queue switching with... oob indexes
    * Given the fact none of these issues provides any logs, I will
      assume they are the same issue fixed in this commit. They
      can be reopened if the issue still persists.
    
    Closes #311
    Closes #307
    Closes #281
    Closes #296

--- app/src/main/java/com/dd3boh/outertune/models/QueueBoard.kt
+++ app/src/main/java/com/dd3boh/outertune/models/QueueBoard.kt
@@ -791,17 +791,17 @@ class QueueBoard(queues: MutableList<MultiQueueObject> = ArrayList()) {
         masterIndex = masterQueues.indexOf(item)
 
         // if requested to get shuffled queue
-        val mediaItems: MutableList<MediaMetadata>?
-        if (item.shuffled) {
-            mediaItems = item.queue
+        val mediaItems: MutableList<MediaMetadata> = if (item.shuffled) {
+            item.queue
         } else {
-            mediaItems = item.unShuffled
+            item.unShuffled
         }
 
         /**
          * current playing == jump target, do seamlessly
          */
-        val seamlessSupported = player.player.currentMetadata?.id == mediaItems[queuePos].id
+        val seamlessSupported = player.player.currentMetadata?.id == mediaItems[queuePos].id &&
+                item.queuePos >= 0 && item.queuePos < mediaItems.size
         if (seamlessSupported) {
             if (queuePos == 0) {
                 val playerIndex = player.player.currentMediaItemIndex
