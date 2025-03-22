7995d861217d486f4de9d0021627a35f4fbd8f8c
mikooomich <mikooomichael@gmail.com>
2025-03-22 12:01:52 -0400

    DNM: Reapply "app: Keep OuterTune class names"
    This reverts commit b3dc582b1ed06d7e6afa0b59a779223ae7c49f9b.

--- app/src/main/java/com/dd3boh/outertune/utils/potoken/PoTokenWebView.kt
+++ app/src/main/java/com/dd3boh/outertune/utils/potoken/PoTokenWebView.kt
@@ -47,6 +47,7 @@ class PoTokenWebView private constructor(
         //noinspection SetJavaScriptEnabled we want to use JavaScript!
         webViewSettings.javaScriptEnabled = true
         webViewSettings.safeBrowsingEnabled = false
+
         webViewSettings.userAgentString = USER_AGENT
         webViewSettings.blockNetworkLoads = true // the WebView does not need internet access
 
