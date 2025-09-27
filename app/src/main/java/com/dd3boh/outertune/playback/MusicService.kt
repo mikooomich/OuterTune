33f471e998bbedb4d14c67e9b9c2f2612da182dd
Michael Zh <mikooomichael@gmail.com>
2025-09-26 22:56:03 -0400

    FDROID: Drop media3 submodule

--- app/src/main/java/com/dd3boh/outertune/playback/MusicService.kt
+++ app/src/main/java/com/dd3boh/outertune/playback/MusicService.kt
@@ -760,12 +760,10 @@ class MusicService : MediaLibraryService(),
             return object : NextRenderersFactory(this@MusicService) {
                 override fun buildAudioSink(
                     context: Context,
-                    pcmEncodingRestrictionLifted: Boolean,
                     enableFloatOutput: Boolean,
                     enableAudioTrackPlaybackParams: Boolean
                 ): AudioSink? {
                     return DefaultAudioSink.Builder(this@MusicService)
-                        .setPcmEncodingRestrictionLifted(pcmEncodingRestrictionLifted)
                         .setEnableAudioTrackPlaybackParams(enableAudioTrackPlaybackParams)
                         .setAudioProcessorChain(
                             DefaultAudioSink.DefaultAudioProcessorChain(
@@ -784,12 +782,10 @@ class MusicService : MediaLibraryService(),
             return object : DefaultRenderersFactory(this) {
                 override fun buildAudioSink(
                     context: Context,
-                    pcmEncodingRestrictionLifted: Boolean,
                     enableFloatOutput: Boolean,
                     enableAudioTrackPlaybackParams: Boolean
                 ): AudioSink? {
                     return DefaultAudioSink.Builder(this@MusicService)
-                        .setPcmEncodingRestrictionLifted(pcmEncodingRestrictionLifted)
                         .setEnableAudioTrackPlaybackParams(enableAudioTrackPlaybackParams)
                         .setAudioProcessorChain(
                             DefaultAudioSink.DefaultAudioProcessorChain(
